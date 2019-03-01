23997f58530f7ce5704e16af42fec60b755a613e
asdf1899 <araid.anas99@gmail.com>
2019-03-01 21:58:46 +0100

    Fix layout

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-function shuffle(a) {
+function shuffle(imgArray) {
     var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
+    for (i = imgArray.length - 1; i > 0; i--) {
         j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+        x = imgArray[i];
+        imgArray[i] = imgArray[j];
+        imgArray[j] = x;
     }
-    return a;
+    return imgArray;
 }
 function removeNine(imgArray){
   for (var j=0;j<imgArray.length;j++){
-    if (imgArray[j] == '9.png'){
+    if (imgArray[j] == '9'){
       imgArray[j] = null;
       return imgArray;
     }
@@ -20,10 +20,25 @@ function init(){
   // fill imgArray with images
   var imgArray = new Array();
   for (var i=0; i < 9;i++){
-    imgArray[i] = i + 1 +'.png';
+    imgArray[i] = i + 1;
   }
   // shuffle array
   imgArray = shuffle(imgArray);
   imgArray = removeNine(imgArray);
-  //jQuery('<img id>')
+  console.log(imgArray);
+  for (var j=0;j<imgArray.length;j++){
+    if (imgArray[j] == null){
+      var img = jQuery('<img id="'+ 0 +'">');
+      img.attr('src', 'img/' +'0.png');
+      img.attr('class', 'style-game-number');
+      img.attr('draggable', 'false');
+      img.appendTo('#game');
+    }else{
+      var img = jQuery('<img id="'+ imgArray[j] +'">');
+      img.attr('src', 'img/' + imgArray[j] + '.png');
+      img.attr('class', 'style-game-number');
+      img.attr('draggable', 'false');
+      img.appendTo('#game');
+    }
+  }
 }
