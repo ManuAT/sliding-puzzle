0d1bc6def0fc54f8e244263adf16d68f0147c384
asdf1899 <araid.anas99@gmail.com>
2019-03-01 23:08:55 +0100

    Add checkPosition

--- js/app.js
+++ js/app.js
@@ -16,16 +16,7 @@ function removeNine(imgArray){
     }
   }
 }
-function init(){
-  // fill imgArray with images
-  var imgArray = new Array();
-  for (var i=0; i < 9;i++){
-    imgArray[i] = i + 1;
-  }
-  // shuffle array
-  imgArray = shuffle(imgArray);
-  imgArray = removeNine(imgArray);
-  console.log(imgArray);
+function addImages(imgArray){
   for (var j=0;j<imgArray.length;j++){
     if (imgArray[j] == null){
       var img = jQuery('<img id="'+ 0 +'">');
@@ -41,4 +32,85 @@ function init(){
       img.appendTo('#game');
     }
   }
+}
+function arrayToMatrix(imgArray){
+  var numberMatrix = [];
+  t=0;
+  tempArray= new Array();
+  for (var j=0; j<imgArray.length;j++){
+    tempArray[t] = imgArray[j];
+    t++;
+    if (t == 3){
+      numberMatrix.push(tempArray);
+      tempArray= new Array();
+      t=0;
+    }
+  }
+  return numberMatrix;
+}
+function getElementPosition(element, matrix){
+  for (var i=0; i<matrix.length;i++){
+    for(var j=0; j<matrix.length;j++){
+      if (matrix[i][j] == element){
+        return new Array(i, j);
+      }
+    }
+  }
+  return false;
+}
+function checkPosition(move, element, matrix){
+  var position = getElementPosition(element, matrix);
+  if (!position){
+    return false;
+  }
+  switch (move) {
+    case 'destra':
+      var j = position[1] + 1;
+      if (j < 3){
+        return true;
+      }
+      return false;
+      break;
+    case 'sinistra':
+      var j = position[1] - 1;
+      if (j >= 0){
+        return true;
+      }
+      return false;
+      break;
+    case 'alto':
+      var i = position[0] - 1;
+      if (i >= 0){
+        return true;
+      }
+      return false;
+      break;
+    case 'basso':
+      var i = position[0] + 1;
+      if (i < 3){
+        return true;
+      }
+      return false;
+      break;
+    default:
+      return false;
+  }
+}
+function init(){
+  // fill imgArray with images
+  var imgArray = new Array();
+  for (var i=0; i < 9;i++){
+    imgArray[i] = i + 1;
+  }
+  // shuffle array
+  imgArray = shuffle(imgArray);
+  // remove 9th element
+  imgArray = removeNine(imgArray);
+  console.log(imgArray);
+  // add image to DOM
+  addImages(imgArray);
+  var matrix = arrayToMatrix(imgArray);
+  console.log(matrix);
+  var position = checkPosition('basso', 5, matrix);
+  console.log(position);
 }
